2efea16bf9f2e8b3fe43d60b71397138f183dd8c
RomeoTodorov <124561100+RomeoTodorov@users.noreply.github.com>
2023-03-06 11:33:42 +0200

    Update Program.cs
    delete comment

--- StatisticArray/StatisticArray/StatisticArray/Program.cs
+++ StatisticArray/StatisticArray/StatisticArray/Program.cs
@@ -8,7 +8,6 @@ namespace StatisticArray
         static void Main(string[] args)
         {
             
-            //Zadacha1
             int n = int.Parse(Console.ReadLine());
             int[] arr = new int[n];
             for (int i = 0; i < n; i++)
